8dc62408b16e2e9a4d64cb92140794ecd15d17af
nemuirari <lyukey0902@gmail.com>
2026-02-06 19:12:24 +0900

    Remove folder from repository but keep it locally

--- study/README.md
+++
@@ -1,3 +0,0 @@
-## The Rust Programming Language: 2018 Edition
-reading at: chapter 3-2 ends, next p64~.
-
--- study/hello_cargo/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let s1 = "こんにちは";
-    let s2 = "AtCoder";
-
-    println!("{}", s1);
-    println!("{}", s2);
-}
