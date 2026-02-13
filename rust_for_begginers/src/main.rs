bdf99995e578ccf3092023132dbffda0231f9266
nemuirari <lyukey0902@gmail.com>
2026-02-13 19:05:38 +0900

    practice: rust

--- rust_for_begginers/src/main.rs
+++ rust_for_begginers/src/main.rs
@@ -1,8 +1,11 @@
 pub mod second;
+pub mod third;
 
 use second::hello;
+use third::third::execute;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, main!");
     hello();
+    execute();
 }
---
+++ rust_for_begginers/src/third/third.rs
@@ -0,0 +1,3 @@
+pub fn execute() {
+    println!("Hello from the third module!");
+}
