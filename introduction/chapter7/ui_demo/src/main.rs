db7baac93db947fe18c501cee208da880dfd2309
nemuirari <lyukey0902@gmail.com>
2026-03-06 09:48:07 +0900

    practice: rust

--- introduction/chapter7/ui_demo/src/main.rs
+++ introduction/chapter7/ui_demo/src/main.rs
@@ -1,15 +1,22 @@
-use iced::{executor, widget::Text, Application, Command, Element, Settings, Theme};
+use iced::widget::{button, column, row, text};
+use iced::{executor, Alignment, Application, Command, Element, Font, Length, Settings, Theme};
 
 struct GUI;
 
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    StartStop,
+    Reset,
+}
+
 impl Application for GUI {
     type Executor = executor::Default;
-    type Message = ();
+    type Message = Message;
     type Flags = ();
     type Theme = Theme;
 
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
-        (GUI, Command::none())
+        (GUI {}, Command::none())
     }
 
     fn title(&self) -> String {
@@ -21,10 +28,33 @@ impl Application for GUI {
     }
 
     fn view(&self) -> Element<'_, Self::Message> {
-        Text::new("Hello, world!").into()
+        let tick_text = text("00:00:00").size(60);
+
+        let start_stop_button =
+            button(text("Start").horizontal_alignment(iced::alignment::Horizontal::Center))
+                .on_press(Message::StartStop)
+                .width(80);
+
+        let reset_button =
+            button(text("Reset").horizontal_alignment(iced::alignment::Horizontal::Center))
+                .on_press(Message::Reset)
+                .width(80);
+
+        column![tick_text, row![start_stop_button, reset_button].spacing(10)]
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Alignment::Center)
+            .into()
     }
 }
 
 fn main() -> iced::Result {
-    GUI::run(Settings::default())
+    let mut settings = Settings::default();
+
+    settings.window.size = (400, 150);
+    settings.default_font = Font::default();
+
+    GUI::run(settings)
 }
